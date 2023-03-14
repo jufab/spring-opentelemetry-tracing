b10570cae67211b293f134ef30cd108066156516
Julien Fabre <julien.fabre.33@gmail.com>
2023-03-14 23:11:36 +0100

    chore: review

--- spring-tracing-opentelemetry/src/main/java/fr/jufab/springtracingopentelemetry/SpringTracingOpentelemetryApplication.java
+++ spring-tracing-opentelemetry/src/main/java/fr/jufab/springtracingopentelemetry/SpringTracingOpentelemetryApplication.java
@@ -1,23 +1,9 @@
 package fr.jufab.springtracingopentelemetry;
 
-import io.opentelemetry.api.OpenTelemetry;
-import io.opentelemetry.api.common.Attributes;
-import io.opentelemetry.context.propagation.ContextPropagators;
-import io.opentelemetry.exporter.otlp.trace.OtlpGrpcSpanExporter;
 import io.opentelemetry.instrumentation.spring.autoconfigure.EnableOpenTelemetry;
-import io.opentelemetry.instrumentation.spring.autoconfigure.SamplerProperties;
-import io.opentelemetry.sdk.OpenTelemetrySdk;
-import io.opentelemetry.sdk.resources.Resource;
-import io.opentelemetry.sdk.trace.SdkTracerProvider;
-import io.opentelemetry.sdk.trace.export.BatchSpanProcessor;
-import io.opentelemetry.sdk.trace.samplers.Sampler;
-import io.opentelemetry.semconv.resource.attributes.ResourceAttributes;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
-
 
 @SpringBootApplication
 @EnableFeignClients
@@ -27,5 +13,4 @@ public class SpringTracingOpentelemetryApplication {
   public static void main(String[] args) {
     SpringApplication.run(SpringTracingOpentelemetryApplication.class, args);
   }
-
 }
--- spring-tracing-otel/src/main/java/fr/jufab/springtracing/SpringTracingOtelApplication.java
+++ spring-tracing-otel/src/main/java/fr/jufab/springtracing/SpringTracingOtelApplication.java
@@ -1,13 +1,8 @@
 package fr.jufab.springtracing;
 
-import io.opentelemetry.api.common.Attributes;
-import io.opentelemetry.sdk.resources.Resource;
-import io.opentelemetry.semconv.resource.attributes.ResourceAttributes;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableFeignClients
@@ -16,16 +11,4 @@ public class SpringTracingOtelApplication {
   public static void main(String[] args) {
     SpringApplication.run(SpringTracingOtelApplication.class, args);
   }
-
-  /**
-   * Service name with value otel.serviceName.
-   * By default, service name is define by spring.application.name properties
-   * @param serviceName
-   * @return Resource
-   */
-/*  @Bean
-  Resource otelResource(@Value("${otel.serviceName}") String serviceName) {
-    return Resource.getDefault()
-        .merge(Resource.create(Attributes.of(ResourceAttributes.SERVICE_NAME, serviceName)));
-  }*/
 }
